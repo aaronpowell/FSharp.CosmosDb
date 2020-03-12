8fd62009e360ef1aae36cb6f1b4e80441be10a5f
Aaron Powell <me@aaron-powell.com>
2020-03-12 17:07:21 +1100

    preparing github actions

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Changelog for `FSharp.CosmosDb`
+
+## [0.1.0] - 2020-03-12
+
+Initial Release :tada:
+
+### Added
+
+- Basic query API
+- Basic Analyzer support
+- Tests
--- build.fsx
+++ build.fsx
@@ -1,32 +1,87 @@
-#r "paket:
-nuget Fake.DotNet.Cli
-nuget Fake.IO.FileSystem
-nuget Fake.Core.Target //"
 #load ".fake/build.fsx/intellisense.fsx"
+
 open Fake.Core
 open Fake.DotNet
 open Fake.IO
-open Fake.IO.FileSystemOperators
 open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
 
-Target.initEnvironment ()
+Target.initEnvironment()
+
+let sln = "./FSharp.CosmosDb.sln"
+
+let getChangelog() =
+    let changelog = "CHANGELOG.md" |> Changelog.load
+    changelog.LatestEntry
+
+let isRelease (targets: Target list) =
+    targets
+    |> Seq.map (fun t -> t.Name)
+    |> Seq.exists ((=) "Release")
+
+let configuration (targets: Target list) =
+    let defaultVal =
+        if isRelease targets then "Release" else "Debug"
+    match Environment.environVarOrDefault "CONFIGURATION" defaultVal with
+    | "Debug" -> DotNet.BuildConfiguration.Debug
+    | "Release" -> DotNet.BuildConfiguration.Release
+    | config -> DotNet.BuildConfiguration.Custom config
 
 Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs 
-)
+    DotNet.exec id "clean" "" |> ignore
+    !!"./.nuget" |> Shell.cleanDirs)
 
-Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build id)
-)
+Target.create "Restore" (fun _ -> DotNet.restore id sln)
+
+Target.create "Build" (fun ctx ->
+    let changelog = getChangelog()
+
+    let args =
+        [ sprintf "/p:PackageVersion=%s" (changelog.NuGetVersion)
+          "--no-restore" ]
+    DotNet.build (fun c ->
+        { c with
+              Configuration = configuration (ctx.Context.AllExecutingTargets)
+              Common = c.Common |> DotNet.Options.withAdditionalArgs args }) sln)
+
+Target.create "Publish" (fun ctx ->
+    let changelog = getChangelog()
+
+    let args =
+        [ sprintf "/p:PackageVersion=%s" (changelog.NuGetVersion)
+          "--no-restore"
+          "--no-build" ]
+    DotNet.publish (fun c ->
+        { c with
+              Configuration = configuration (ctx.Context.AllExecutingTargets)
+              Common = c.Common |> DotNet.Options.withAdditionalArgs args }) sln)
+
+Target.create "Package" (fun ctx ->
+    let changelog = getChangelog()
+
+    let args =
+        [ sprintf "/p:PackageVersion=%s" (changelog.NuGetVersion)
+          sprintf "/p:PackageReleaseNotes=\"%s\"" (sprintf "%O" changelog) ]
+    DotNet.pack (fun c ->
+        { c with
+              Configuration = configuration (ctx.Context.AllExecutingTargets)
+              Common = c.Common |> DotNet.Options.withAdditionalArgs args }) sln)
+
+Target.create "PackageVersion" (fun _ ->
+    let version = getChangelog()
+    printfn "The version is %s" version.NuGetVersion)
+
+Target.create "Changelog" (fun _ ->
+    let changelog = getChangelog()
+    Directory.ensure "./.nupkg"
+
+    [| sprintf "%O" changelog |] |> File.append "./.nupkg/changelog.md")
 
 Target.create "All" ignore
+Target.create "Release" ignore
 
-"Clean"
-  ==> "Build"
-  ==> "All"
+"Clean" ==> "Restore" ==> "Build" ==> "All"
+
+"Clean" ==> "Restore" ==> "Build" ==> "Publish" ==> "Package" ==> "Changelog" ==> "Release"
 
 Target.runOrDefault "All"
