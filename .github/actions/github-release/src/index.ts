2042c2b064f79a4be766ce71e77f3283c5efd3d0
Aaron Powell <me@aaron-powell.com>
2020-03-13 13:58:51 +1100

    trying a full release

---
+++ .github/actions/github-release/src/index.ts
@@ -0,0 +1,69 @@
+import * as core from "@actions/core";
+import * as github from "@actions/github";
+import { lstatSync, readFileSync } from "fs";
+import { mimeOrDefault } from "mime";
+import { basename } from "path";
+import { Context } from "@actions/github/lib/context";
+
+function fileInfo(path: string) {
+  return {
+    name: basename(path),
+    mime: mimeOrDefault(path),
+    size: lstatSync(path).size,
+    file: readFileSync(path)
+  };
+}
+
+async function upload(
+  octokit: github.GitHub,
+  context: Context,
+  url: string,
+  path: string
+) {
+  let { name, mime, size, file } = fileInfo(path);
+  console.log(`Uploading ${name}...`);
+  await octokit.repos.uploadReleaseAsset({
+    ...context.repo,
+    name,
+    file,
+    url,
+    headers: {
+      "content-length": size,
+      "content-type": mime
+    }
+  });
+}
+
+async function run() {
+  const token = core.getInput("token");
+  const sha = core.getInput("sha");
+  const version = core.getInput("version");
+
+  const releaseNotes = readFileSync("changelog.md", { encoding: "UTF8" });
+
+  const octokit = new github.GitHub(token);
+  const context = github.context;
+
+  const release = await octokit.repos.createRelease({
+    ...context.repo,
+    tag_name: version,
+    target_commitish: sha,
+    name: `Release ${version}`,
+    body: releaseNotes
+  });
+
+  await upload(
+    octokit,
+    context,
+    release.data.upload_url,
+    `FSharp.CosmosDb.${version}.nupkg`
+  );
+  await upload(
+    octokit,
+    context,
+    release.data.upload_url,
+    `FSharp.CosmosDb.Analyzer.${version}.nupkg`
+  );
+}
+
+run();
