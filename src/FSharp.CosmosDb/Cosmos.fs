98309d08bcbe2fea3bcd82e1acba513c37d97d46
Aaron Powell <me@aaron-powell.com>
2020-03-17 20:58:15 +1100

    Some refactoring on how the API will be passing stuff around

--- src/FSharp.CosmosDb/Cosmos.fs
+++ src/FSharp.CosmosDb/Cosmos.fs
@@ -3,50 +3,47 @@ namespace FSharp.CosmosDb
 open Azure.Cosmos
 open FSharp.Control
 
-type CosmosOperation =
+type ConnectionOperation =
     { Options: CosmosClientOptions option
       FromConnectionString: bool
       Endpoint: string option
       AccessKey: string option
       ConnectionString: string option
       DatabaseId: string option
-      ContainerName: string option
+      ContainerName: string option }
+
+type QueryOperation =
+    { Connection: ConnectionOperation
       Query: string option
       Parameters: (string * obj) list }
 
 module Cosmos =
-    let fromConnectionString connString =
+    let private defaultConnectionOp =
         { Options = None
-          FromConnectionString = true
-          Endpoint = None
-          AccessKey = None
-          ConnectionString = connString
-          DatabaseId = None
-          ContainerName = None
-          Query = None
-          Parameters = List.empty }
-
-    let fromConnectionStringWithOptions connString op =
-        { Options = Some op
-          FromConnectionString = true
-          Endpoint = None
-          AccessKey = None
-          ConnectionString = connString
-          DatabaseId = None
-          ContainerName = None
-          Query = None
-          Parameters = List.empty }
-
-    let host endpoint =
-        { Options = None
-          Endpoint = Some endpoint
           FromConnectionString = false
+          Endpoint = None
           AccessKey = None
           ConnectionString = None
           DatabaseId = None
-          ContainerName = None
+          ContainerName = None }
+
+    let private defaultQueryOp =
+        { Connection = defaultConnectionOp
           Query = None
-          Parameters = List.empty }
+          Parameters = [] }
+
+    let fromConnectionString connString =
+        { defaultConnectionOp with
+              FromConnectionString = true
+              ConnectionString = connString }
+
+    let fromConnectionStringWithOptions connString op =
+        { defaultConnectionOp with
+              Options = Some op
+              FromConnectionString = true
+              ConnectionString = connString }
+
+    let host endpoint = { defaultConnectionOp with Endpoint = Some endpoint }
 
     let connectWithOptions options accessKey op =
         { op with
@@ -59,25 +56,29 @@ module Cosmos =
 
     let container cn op = { op with ContainerName = Some cn }
 
-    let query query op = { op with Query = Some query }
+    let query query op =
+        { defaultQueryOp with
+              Query = Some query
+              Connection = op }
 
     let parameters arr op = { op with Parameters = arr }
 
     let execAsync<'T> op =
+        let connInfo = op.Connection
         let clientOps =
-            match op.Options with
+            match connInfo.Options with
             | Some op -> op
             | None -> CosmosClientOptions()
 
         let client =
-            if op.FromConnectionString then
+            if connInfo.FromConnectionString then
                 maybe {
-                    let! connStr = op.ConnectionString
+                    let! connStr = connInfo.ConnectionString
                     return new CosmosClient(connStr, clientOps) }
             else
                 maybe {
-                    let! host = op.Endpoint
-                    let! accessKey = op.AccessKey
+                    let! host = connInfo.Endpoint
+                    let! accessKey = connInfo.AccessKey
                     return new CosmosClient(host, accessKey, clientOps) }
 
         match client with
@@ -85,8 +86,8 @@ module Cosmos =
         | Some client ->
             let result =
                 maybe {
-                    let! databaseId = op.DatabaseId
-                    let! containerName = op.ContainerName
+                    let! databaseId = connInfo.DatabaseId
+                    let! containerName = connInfo.ContainerName
 
                     let db = client.GetDatabase databaseId
                     let container = db.GetContainer containerName
