b5d4ce5e544187a5cf3d8b194c8e20b610fcf185
Aaron Powell <me@aaron-powell.com>
2020-03-17 16:58:09 +1100

    Moving maybe binder to be shared

--- src/FSharp.CosmosDb/MaybeBinder.fs
+++
@@ -1,9 +0,0 @@
-[<AutoOpen>]
-module MaybeBinder
-
-type MaybeBinder() =
-    member __.Bind(value, binder) = Option.bind binder value
-
-    member __.Return value = Some value
-
-let maybe = MaybeBinder()
