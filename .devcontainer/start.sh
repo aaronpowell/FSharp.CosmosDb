3c11a84c3e12950f09765b79ae916c2ca847f499
Aaron Powell <me@aaron-powell.com>
2022-08-22 06:01:44 +0000

    installing emulator cert and setting up the app config

--- .devcontainer/start.sh
+++ .devcontainer/start.sh
@@ -2,6 +2,8 @@
 
 set -euxo pipefail
 
+COSMOS__ENDPOINT=https://$(docker inspect cosmos -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}'):8081
+
 # Try to get the emulator cert in a loop
 until sudo curl -ksf "${COSMOS__ENDPOINT}/_explorer/emulator.pem" -o '/usr/local/share/ca-certificates/emulator.crt'; do
   echo "Downloading cert from $COSMOS__ENDPOINT"
@@ -9,3 +11,5 @@ until sudo curl -ksf "${COSMOS__ENDPOINT}/_explorer/emulator.pem" -o '/usr/local
 done
 
 sudo update-ca-certificates
+
+echo '{ "Cosmos": { "EndPoint" : "'$COSMOS__ENDPOINT'" } }' >> ./samples/FSharp.CosmosDb.Samples/appsettings.Development.json
